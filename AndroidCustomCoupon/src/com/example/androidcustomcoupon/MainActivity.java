ebf0252487085f6002624cbe95f35aa42df82d5d
MV <miaowei_email@163.com>
2016-07-13 17:02:55 +0800

    自定义View(优惠券样式)

---
+++ AndroidCustomCoupon/src/com/example/androidcustomcoupon/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidcustomcoupon;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * 
+ * 生成日期：2016-7-13 <br/>
+ * 描述：自定义优惠券
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
